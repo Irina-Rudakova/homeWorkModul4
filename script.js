f116e214edef54590add8d8241dbf9cd0cb08b5e
Irina Rudakova <irina44.photographer@gmail.com>
2024-08-12 23:51:04 +0300

    homework

---
+++ script.js
@@ -0,0 +1,57 @@
+/*alert("Спокойной ночи!")*/ 
+/*document.addEventListener("DOMContentLoaded", )*/
+/*document.addEventListener("load", )*/
+/*let userName*/
+
+/*let user
+user = "Bob";
+alert(user)
+
+let age = 18
+alert(age)*/
+
+/*let user="Jonh",
+    age=30;
+
+    user="Alex"
+
+alert(user)
+alert(age)*/
+
+
+/*let user="Jonh",
+    age=30;
+
+user="Alex";
+alert(`Ваше имя ${user}`);
+alert(age);*/
+
+/*const user="Mari";
+
+alert(user);
+
+let и const. let – изменяемое значение переменной, const – неизменяемое.*/
+
+/*var user="Leo";
+user="Nik";
+alert(user);*/
+
+/*var user1="Leo";
+let user2="Nik";
+
+alert(user1);
+alert(user2);
+
+Получается, когда мы создаем переменную с ключевым словом var, 
+у нас ячейка памяти под таким именем резервируется. И как только встречает данные создания переменной, 
+туда кладется значение. Если же мы создаем переменную с ключевым словом let,
+ то у нас создается ячейка памяти только в том случае, когда у нас встречается данная строка.*/
+
+
+ alert(user1)
+ var user1="Leo";
+ alert(user1)
+
+ alert(user1)
+ let user1="Leo";
+ alert(user1)
